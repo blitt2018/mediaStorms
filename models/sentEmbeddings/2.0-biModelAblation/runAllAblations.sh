6ea7791505124125dfc9349f0b30f8a6b76427d7
Benjamin Roger Litterer <blitt@curry.si.umich.edu>
2023-10-23 15:26:54 -0400

    camera ready figures

--- models/sentEmbeddings/2.0-biModelAblation/runAllAblations.sh
+++ models/sentEmbeddings/2.0-biModelAblation/runAllAblations.sh
@@ -1,4 +1,6 @@
-#python3 384_0/1.2-bl-biEncoderHead.py
+#run all ablations for the bi-encoder model 
+
+python3 384_0/1.2-bl-biEncoderHead.py
 python3 192_192/1.1-bl-biEncoder_192_192.py
 python3 288_96/1.0-bl-biEncoder_288_96.py
-#justEn/1.3-bl-biEncoder_en.py
+justEn/1.3-bl-biEncoder_en.py
